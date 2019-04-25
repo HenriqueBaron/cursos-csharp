7e2608974865e86e0660a011a6a22c69335d7bea
Henrique Baron <henrique.baron@gmail.com>
2019-04-25 18:56:52 -0300

    Criar classe Calculadora

---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.Util/Calculadora.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinaWeb.Calculadora.Util
+{
+    public class Calculadora
+    {
+        private int limiteMinimo;
+
+        public Calculadora(int limiteMinimo = 0)
+        {
+            this.limiteMinimo = limiteMinimo;
+        }
+
+        public int Somar(int a, int b)
+        {
+            if ((a < limiteMinimo) || (b < limiteMinimo))
+            {
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite mínimo");
+            }
+            return a + b;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            if ((a < limiteMinimo) || (b < limiteMinimo))
+            {
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite esperado");
+            }
+            return a - b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Dividir(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
