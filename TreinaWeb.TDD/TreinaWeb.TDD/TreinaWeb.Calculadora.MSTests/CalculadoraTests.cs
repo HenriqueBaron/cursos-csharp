5fd87d565b218b03e27c2c8656ebd380ddb348b4
Henrique Baron <henrique.baron@gmail.com>
2019-04-25 22:33:54 -0300

    Adicionar projeto de teste de unidade com o MSTest

---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.MSTests/CalculadoraTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwCalc = TreinaWeb.Calculadora.Util;
+
+namespace TreinaWeb.Calculadora.MSTests
+{
+    [TestClass]
+    public class CalculadoraTests
+    {
+        private TwCalc.Calculadora calc;
+
+        [TestInitialize]
+        public void SetUp() {
+            calc = new TwCalc.Calculadora();
+        }
+
+        [TestCleanup]
+        public void CleanUp() {
+            calc = null;
+        }
+
+        [TestMethod]
+        public void TestSomar() {
+            Assert.AreEqual(4, calc.Somar(2, 2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void SomarComException() {
+            TwCalc.Calculadora calc = new TwCalc.Calculadora(2);
+            calc.Somar(1, 1);
+            /* Outro jeito de esperar por uma exceção no código de teste seria
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => calc.Somar(1, 1));
+
+            Neste caso, o atributo ExpectedException não seria necessário.
+            */
+        }
+
+        [TestMethod]
+        public void TestSubtrair() {
+            Assert.AreEqual(0, calc.Subtrair(2, 2));
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TestMultiplicar() {
+
+        }
+    }
+}
---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.MSTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TreinaWeb.Calculadora.MSTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TreinaWeb.Calculadora.MSTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("644f9935-b8c0-4048-93dd-28ffd06bf3ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
