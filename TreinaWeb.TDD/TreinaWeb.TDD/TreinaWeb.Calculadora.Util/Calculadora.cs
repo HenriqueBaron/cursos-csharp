c90f25f81d87dd708b0101cb5920911ce0fe80a2
Henrique Baron <henrique.baron@gmail.com>
2019-04-27 10:23:08 -0300

    Validar os argumentos da operação em um método separado

--- TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.Util/Calculadora.cs
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.Util/Calculadora.cs
@@ -15,31 +15,29 @@ namespace TreinaWeb.Calculadora.Util
         }
 
         public int Somar(int a, int b) {
-            if ((a < limiteMinimo) || (b < limiteMinimo)) {
-                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite mínimo");
-            }
+            VerificarLimiteArgumentos(a, b);
             return a + b;
         }
 
         public int Subtrair(int a, int b) {
-            if ((a < limiteMinimo) || (b < limiteMinimo)) {
-                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite esperado");
-            }
+            VerificarLimiteArgumentos(a, b);
             return a - b;
         }
 
         public int Multiplicar(int a, int b) {
-            if ((a < limiteMinimo) || (b < limiteMinimo)) {
-                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite esperado");
-            }
+            VerificarLimiteArgumentos(a, b);
             return a * b;
         }
 
         public int Dividir(int a, int b) {
-            if ((a < limiteMinimo) || (b < limiteMinimo)) {
-                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite esperado");
-            }
+            VerificarLimiteArgumentos(a, b);
             return a / b;
+        }
+
+        private void VerificarLimiteArgumentos(int a, int b) {
+            if ((a < limiteMinimo) || (b < limiteMinimo)) {
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite mínimo");
+            }
         }
     }
 }
