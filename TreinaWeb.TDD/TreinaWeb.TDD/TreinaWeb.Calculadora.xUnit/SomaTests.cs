8315d4186ec0cbb8a6f0bec9f5fecee0271987d6
Henrique Baron <henrique.baron@gmail.com>
2019-04-27 09:49:39 -0300

    Testar (no xUnit) soma e subtração em classes separadas.

---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.xUnit/SomaTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TreinaWeb.Calculadora.xUnit
+{
+    public class SomaTests
+    {
+        Util.Calculadora calc;
+
+        public SomaTests() {
+            calc = new Util.Calculadora();
+        }
+
+        [Fact]
+        public void TestSomaNumerosPositivos() {
+            Assert.Equal(4, calc.Somar(2, 2));
+        }
+
+        [Fact]
+        public void TestSomaAbaixoLimitePrimeiroArgumento() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Somar(-1, 0));
+        }
+
+        [Fact]
+        public void TestSomaAbaixoLimiteSegundoArgumento() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Somar(0, -1));
+        }
+
+        [Fact]
+        public void TestSomaAbaixoLimiteAmbosArgumentos() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Somar(-1, -1));
+        }
+    }
+}
---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.xUnit/SubtrairTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TreinaWeb.Calculadora.xUnit
+{
+    public class SubtrairTests
+    {
+        Util.Calculadora calc;
+
+        public SubtrairTests() {
+            calc = new Util.Calculadora();
+        }
+
+        [Fact]
+        public void TestSubtracaoNumerosPositivos() {
+            Assert.Equal(-1, calc.Subtrair(3, 4));
+        }
+
+        [Fact]
+        public void TestSubtracaoAbaixoLimitePrimeiroArgumento() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Subtrair(-1, 1));
+        }
+
+        [Fact]
+        public void TestSubtracaoAbaixoLimiteSegundoArgumento() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Subtrair(1, -1));
+        }
+
+        [Fact]
+        public void TestSubtracaoAbaixoLimiteAmbosArgumentos() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Subtrair(-1, -1));
+        }
+    }
+}
