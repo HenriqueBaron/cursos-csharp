9eafeb5a491ac55ebb340bafa9d030a69cf95b1f
Henrique Baron <henrique.baron@gmail.com>
2019-04-27 10:07:40 -0300

    Implementar os testes de unidade para a multiplicação

---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.xUnit/MultiplicacaoTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TreinaWeb.Calculadora.xUnit
+{
+    public class MultiplicacaoTests : IDisposable
+    {
+        Util.Calculadora calc;
+
+        public MultiplicacaoTests() {
+            calc = new Util.Calculadora(2);
+        }
+
+        public void Dispose() {
+            calc = null;
+        }
+
+        [Fact]
+        public void TestMultiplicacao() {
+            Assert.Equal(9, calc.Multiplicar(3, 3));
+        }
+
+        [Fact]
+        public void TestMultiplicacaoPrimeiroArgumentoAbaixoLimite() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Multiplicar(1, 3));
+        }
+
+        [Fact]  
+        public void TestMultiplicacaoSegundoArgumentoAbaixoLimite() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Multiplicar(3, 1));
+        }
+
+        [Fact]  
+        public void TestMultiplicacaoAmbosArgumentosAbaixoLimite() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Multiplicar(1, 1));
+        }
+    }
+}
