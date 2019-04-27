689b31eb57ca284042f0e18a110a6110992470b3
Henrique Baron <henrique.baron@gmail.com>
2019-04-27 10:15:39 -0300

    Adicionar os testes de unidade para a divisão

---
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.xUnit/DividirTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TreinaWeb.Calculadora.xUnit
+{
+    public class DividirTests
+    {
+        Util.Calculadora calc;
+
+        public DividirTests() {
+            calc = new Util.Calculadora();
+        }
+
+        [Fact]  
+        public void TestDivisao() {
+            Assert.Equal(3, calc.Dividir(9, 3));
+        }
+
+        [Fact]
+        public void TestDivisaoAbaixoLimitePrimeiroArgumento() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Dividir(-1, 3));
+        }
+
+        [Fact]  
+        public void TestDivisaoAbaixoLimiteSegundoArgumento() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Dividir(9, -3));
+        }
+
+        [Fact]
+        public void TestDivisaoAbaixoLimiteAmbosArgumentos() {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calc.Dividir(-3, -3));
+        }
+    }
+}
