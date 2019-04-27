e668a4afca3defdfd5d0e5f4e6dad2ff6f4d1739
Henrique Baron <henrique.baron@gmail.com>
2019-04-27 10:15:52 -0300

    Implementar o método de divisão

--- TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.Util/Calculadora.cs
+++ TreinaWeb.TDD/TreinaWeb.TDD/TreinaWeb.Calculadora.Util/Calculadora.cs
@@ -30,13 +30,16 @@ namespace TreinaWeb.Calculadora.Util
 
         public int Multiplicar(int a, int b) {
             if ((a < limiteMinimo) || (b < limiteMinimo)) {
-                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite mínimo");
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite esperado");
             }
             return a * b;
         }
 
         public int Dividir(int a, int b) {
-            throw new NotImplementedException();
+            if ((a < limiteMinimo) || (b < limiteMinimo)) {
+                throw new ArgumentOutOfRangeException("O argumento está abaixo do limite esperado");
+            }
+            return a / b;
         }
     }
 }
